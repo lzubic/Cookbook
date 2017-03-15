fbe4d7d27ae52093de93c06000d1191bebd8154a
lzubic <lzubic@hotmail.com>
2017-03-15 14:07:45 +0100

    Created User entity model

---
+++ src/main/java/com/cookbook/model/User.java
@@ -0,0 +1,11 @@
+package com.cookbook.model;
+
+public class User {
+    public int id;
+    public String username;
+    public String password;
+    public String firstName;
+    public String lastName;
+
+    public User() {}
+}
