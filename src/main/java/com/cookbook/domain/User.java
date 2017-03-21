77ad3dc033127341f40b81b5af5e37901c1c458a
lzubic <lzubic@hotmail.com>
2017-03-21 19:26:07 +0100

    Added getter and setter methods in User entity

--- src/main/java/com/cookbook/domain/User.java
+++ src/main/java/com/cookbook/domain/User.java
@@ -5,11 +5,51 @@ import org.neo4j.ogm.annotation.NodeEntity;
 
 @NodeEntity
 public class User {
-    @GraphId public int id;
-    public String username;
-    public String password;
-    public String firstName;
-    public String lastName;
+    @GraphId private int id;
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
 
     public User() {}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 }
