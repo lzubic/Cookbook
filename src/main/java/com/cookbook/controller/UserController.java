627b21e436fcc60078c49e8ee10c2a73d5a1765e
lzubic <lzubic@hotmail.com>
2017-03-21 20:23:59 +0100

    Created repository package

--- src/main/java/com/cookbook/controller/UserController.java
+++ src/main/java/com/cookbook/controller/UserController.java
@@ -1,7 +1,5 @@
 package com.cookbook.controller;
 
-import com.cookbook.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
