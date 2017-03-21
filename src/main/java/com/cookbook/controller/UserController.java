9582919e00ff814d3cac38698d6e2c7ea5398d06
lzubic <lzubic@hotmail.com>
2017-03-21 20:40:17 +0100

    Added reference to UserRepository in UserController

--- src/main/java/com/cookbook/controller/UserController.java
+++ src/main/java/com/cookbook/controller/UserController.java
@@ -1,11 +1,20 @@
 package com.cookbook.controller;
 
+import com.cookbook.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
+    private UserRepository userRepo;
+
+    @Autowired
+    public UserController(UserRepository userRepo) {
+        this.userRepo = userRepo;
+    }
+
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String index() {
         return "Index page";
