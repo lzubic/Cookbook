a9a4ee64051b677a61dff6479fe31da286f46b8f
lzubic <lzubic@hotmail.com>
2017-04-02 13:20:26 +0200

    Created ApplicationInitializer initialization class

---
+++ src/main/java/com/cookbook/configuration/ApplicationInitializer.java
@@ -0,0 +1,20 @@
+package com.cookbook.configuration;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+public class ApplicationInitializer implements WebApplicationInitializer {
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+        context.setServletContext(servletContext);
+        ServletRegistration.Dynamic servlet = servletContext.addServlet("Dispatcher", new DispatcherServlet(context));
+        servlet.setLoadOnStartup(1);
+        servlet.addMapping("/");
+    }
+}
