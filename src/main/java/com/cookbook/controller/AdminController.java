d85cb27ceeb74cfe470b0c28933b8317b61c9bfb
lzubic <lzubic@hotmail.com>
2017-06-19 19:42:55 +0200

    Implemented functionality for adding new recipe

--- src/main/java/com/cookbook/controller/AdminController.java
+++ src/main/java/com/cookbook/controller/AdminController.java
@@ -1,7 +1,10 @@
 package com.cookbook.controller;
 
+import com.cookbook.domain.Recipe;
+import com.cookbook.service.RecipeService;
 import com.cookbook.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,30 +13,34 @@ import org.springframework.web.servlet.ModelAndView;
 @RestController
 public class AdminController {
     private final UserService userService;
+    private final RecipeService recipeService;
 
     @Autowired
-    public AdminController(UserService userService) {
+    public AdminController(UserService userService, RecipeService recipeService) {
         this.userService = userService;
+        this.recipeService = recipeService;
     }
 
     @RequestMapping(value = "/admin/dashboard", method = RequestMethod.GET)
     public ModelAndView users() {
-        ModelAndView model = new ModelAndView();
-        model.setViewName("admin/dashboard");
-        return model;
+        ModelAndView mav = new ModelAndView();
+        mav.addObject("recipe", new Recipe());
+        mav.setViewName("admin/home");
+        return mav;
     }
 
     @RequestMapping(value = "/admin/recipes", method = RequestMethod.GET)
     public ModelAndView recipes() {
-        ModelAndView model = new ModelAndView();
-        model.setViewName("admin/recipes/recipes-list");
-        return model;
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("admin/recipes/recipes-list");
+        return mav;
     }
 
     @RequestMapping(value = "/admin/recipes/new", method = RequestMethod.POST)
-    public ModelAndView addNewRecipe() {
-        ModelAndView model = new ModelAndView();
-        model.setViewName("admin/recipes/recipe-form");
-        return model;
+    public ModelAndView createNewRecipe(@ModelAttribute("recipe")Recipe recipe) {
+        recipeService.save(recipe);
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("admin/recipes/recipes-list");
+        return mav;
     }
 }
---
+++ src/main/java/com/cookbook/service/RecipeService.java
@@ -0,0 +1,26 @@
+package com.cookbook.service;
+
+import com.cookbook.domain.Recipe;
+import com.cookbook.repository.RecipeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class RecipeService {
+    private final RecipeRepository recipeRepository;
+
+    @Autowired
+    public RecipeService(RecipeRepository recipeRepository) {
+        this.recipeRepository = recipeRepository;
+    }
+
+    public Iterable<Recipe> getAllRecipes() {
+        return recipeRepository.findAll();
+    }
+
+    public void save(Recipe recipe) {
+        recipeRepository.save(recipe);
+    }
+}
